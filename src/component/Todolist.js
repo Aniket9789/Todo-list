539500728e3364717f66d4c3dacd03fe3a396419
Aniket9789 <117363533+Aniket9789@users.noreply.github.com>
2023-04-29 20:33:45 +0530

    initial commet

---
+++ src/component/Todolist.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todolist(props) {
+  return (
+    <li className="list-item">
+        {props.item}
+        <span className="icons">
+        <i className='bx bxs-trash icon-delete' onClick={e=>{props.deleteItem(props.index)}}></i>
+        </span>
+    </li>
+  )
+}
+
+export default Todolist
